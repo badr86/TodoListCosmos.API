163c29fbb38e26b009ee3479a46656681f52890f
Mohamed Badr <Mohamed.Badr@integrant.com>
2021-12-13 11:00:03 +0200

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace TodoListCosmos.API.Models
+{
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("isComplete")]
+        public bool Completed { get; set; }
+    }
+}
---
+++ Services/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace TodoListCosmos.API.Services
+{
+    public static class DependencyInjection
+    {
+        public static async Task<IServiceCollection> AddCosmosDbService(this IServiceCollection collection, IConfiguration Configuration)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (Configuration == null) throw new ArgumentNullException(nameof(Configuration));
+
+            var configurationSection = Configuration.GetSection("CosmosDb");
+            var databaseName = configurationSection["DatabaseName"];
+            var containerName = configurationSection["ContainerName"];
+            var account = configurationSection["Account"];
+            var key = configurationSection["Key"];
+
+            var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            var cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            collection.AddSingleton<ICosmosDbService>(cosmosDbService);
+            return collection;
+        }
+    }
+}
